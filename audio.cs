78841918165bea7b7d95859c36ff805aefcc677c
Genie_Lab <rlawksml@gmail.com>
2019-11-25 23:06:41 +0900

    audio
    미션에 맞는 audio 사운드 추가 코드

---
+++ audio.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sound : MonoBehaviour
+{
+    public AudioClip Tfood;
+    public AudioClip Ffood;
+    AudioSource aud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.aud = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "coffee1"){
+            this.aud.PlayOneShot(this.Tfood);
+        }
+        else if(other.gameObject.tag == "coffee2"){
+            this.aud.PlayOneShot(this.Tfood);
+        }
+        else if(other.gameObject.tag == "coffee3"){
+            this.aud.PlayOneShot(this.Tfood);
+        }
+        else{
+            this.aud.PlayOneShot(this.Ffood);
+        }
+        Destroy(other.gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray = camera.main.screenPointToRay(Input.mousePosition);
+            PaycastHit hit;
+            if(Physics.Raycast(ra,out hit, Mathf.infinity)) {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x,0,z);
+            }
+        }
+    }
+
+
+}
